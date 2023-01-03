b0c4531e39b547fd9068d35650786efa053bd53f
ARASH FAD <fadarash@gmail.com>
2023-01-03 12:44:45 +0330

    again i remove variables and use netlify variable options

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    MONGODB_USERNAME: "max-next-project",
-    MONGODB_PASSWORD: 123,
-    MONGODB_CLUSTER_NAME: "cluster0",
-    MONGODB_DATABASE: "all_posts",
-  },
 };
 
 module.exports = nextConfig;
