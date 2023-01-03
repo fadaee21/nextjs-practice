d4e511762cd9ee7af115a9d51fd9412a0840f073
ARASH FAD <fadarash@gmail.com>
2023-01-03 12:32:20 +0330

    modify variable just for netlify that read from github

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    MONGODB_USERNAME: "max-next-project",
+    MONGODB_PASSWORD: 123,
+    MONGODB_CLUSTER_NAME: "cluster0",
+    MONGODB_DATABASE: "all_posts",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
